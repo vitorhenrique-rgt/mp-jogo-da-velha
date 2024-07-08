620db676176efe9b8dc6f8774efa2f3a6e7248d5
Vitor Henrique <vitorhenrique_rgt@outlook.com.br>
2024-07-08 19:00:35 -0300

    Criando front-end

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,21 @@
 // ESCREVA SUA LÓGICA AQUI
+const boardCells = document.getElementsByClassName('board-cell');
 
 function main() {
-  alert("Bem vindo ao jogo da velha!");
+  //alert("Bem vindo ao jogo da velha!");
+}
+
+function selectCell() {
+  // Itera sobre cada elemento 'board-cell'.
+  console.log('chamou funçao')
+  for (let cell of boardCells) {
+    cell.addEventListener('click', () => {
+      console.log(cell);
+      console.log('clicou')
+    });
+  }
 }
 
 window.onload = () => {
-  main();
+  selectCell();
 };
